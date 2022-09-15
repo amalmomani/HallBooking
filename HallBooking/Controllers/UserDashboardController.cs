6520943dc4fbe0754e2e83d22ab23fa631617d3f
amalmomani99@yahoo.com <amalmomani99@yahoo.com>
2022-09-15 16:05:32 +0300

    aa

--- HallBooking/Controllers/UserDashboardController.cs
+++ HallBooking/Controllers/UserDashboardController.cs
@@ -1,5 +1,6 @@
 ﻿using HallBooking.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,10 +17,19 @@ namespace HallBooking.Controllers
             _context = context;
             //assign initial value variable
         }
-        public IActionResult Index(int Categoryid)
+        public IActionResult Index(int id)
         {
-            var holls = _context.Halls.Where(x => x.Hallid == Categoryid);
+            var holls = _context.Halls.Where(x => x.Categoryid == id);
             return View(holls);
+        }
+        public async Task<IActionResult> Category()
+        {
+            return View(await _context.Hallcategories.ToListAsync());
+        }
+        public async Task<IActionResult> Halls()
+        {
+            var modelContext = _context.Halls.Include(h => h.Category);
+            return View(await modelContext.ToListAsync());
         }
     }
 }
